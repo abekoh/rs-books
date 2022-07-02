7b7f351494a69a5cda6b9bdd22ade9fdd370baa7
Kotaro Abe <abekoh.bcky@gmail.com>
2022-07-02 23:06:41 +0900

    feat: update

--- books_api/src/feature/book/rest_controller.rs
+++ books_api/src/feature/book/rest_controller.rs
@@ -5,13 +5,14 @@ use serde::{Deserialize, Serialize};
 use url::Url;
 use uuid::Uuid;
 
-use crate::feature::book::ports::{Book, BookCreateInput, BookService};
+use crate::feature::book::ports::{BookCreateInput, BookService, BookUpdateInput};
 
 pub fn configure<T: 'static + BookService>(service: web::Data<T>, cfg: &mut web::ServiceConfig) {
     cfg.app_data(service);
     cfg.route("/books", web::post().to(register::<T>));
     cfg.route("/books", web::get().to(get_all::<T>));
     cfg.route("/books/{id}", web::get().to(get_one::<T>));
+    cfg.route("/books/{id}", web::put().to(update::<T>));
 }
 
 async fn register<T: BookService>(service: web::Data<T>, body: Json<BookCreateInput>) -> impl Responder {
@@ -49,6 +50,19 @@ async fn get_one<T: BookService>(service: web::Data<T>, path: Path<GetOnePath>) 
     let res = service.get_one(&path.id).await;
     match res {
         Ok(b) => HttpResponse::Ok().json(b),
+        Err(err) => match err {
+            e => {
+                warn!("{}", e);
+                HttpResponse::InternalServerError().finish()
+            }
+        }
+    }
+}
+
+async fn update<T: BookService>(service: web::Data<T>, path: Path<GetOnePath>, body: Json<BookUpdateInput>) -> impl Responder {
+    let res = service.update(&path.id, &body).await;
+    match res {
+        Ok(_) => HttpResponse::NoContent().finish(),
         Err(err) => match err {
             e => {
                 warn!("{}", e);
