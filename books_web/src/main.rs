8519040960465c9f3d3cee6abcac0082b9a93072
Kotaro Abe <abekoh.bcky@gmail.com>
2022-07-09 13:49:36 +0900

    feat: url, uuid

--- books_web/src/main.rs
+++ books_web/src/main.rs
@@ -1,13 +1,13 @@
 use serde::Deserialize;
-// use url::Url;
-// use uuid::Uuid;
+use url::Url;
+use uuid::Uuid;
 use yew::prelude::*;
 
 #[derive(Clone, PartialEq, Deserialize)]
 struct Book {
-    // pub id: Uuid,
+    pub id: Uuid,
     pub name: String,
-    // pub url: Option<Url>,
+    pub url: Option<Url>,
 }
 
 #[derive(Properties, PartialEq)]
@@ -28,10 +28,14 @@ fn books_list(BookListProps { books }: &BookListProps) -> Html {
 fn app() -> Html {
     let books: Vec<Book> = vec![
         Book {
+            id: Uuid::new_v4(),
             name: String::from("hoge"),
+            url: None,
         },
         Book {
+            id: Uuid::new_v4(),
             name: String::from("fuga"),
+            url: None,
         },
     ];
     html! {
