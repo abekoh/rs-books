4b85215014fdbb26fed153afb4c4da82bda4f9ed
Kotaro Abe <abekoh.bcky@gmail.com>
2022-07-09 13:50:51 +0900

    chore: change port

--- books_api/src/main.rs
+++ books_api/src/main.rs
@@ -24,7 +24,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .configure(|cfg| configure_features(pg_pool.clone(), cfg))
     })
-        .bind(("127.0.0.1", 8080))?
+        .bind(("127.0.0.1", 8090))?
         .run()
         .await
 }
