b422b874878a9e666b77b6f7fab40b4721123ac3
Kotaro Abe <abekoh.bcky@gmail.com>
2022-07-09 15:32:57 +0900

    chore: error message

--- books_web/src/main.rs
+++ books_web/src/main.rs
@@ -89,7 +89,7 @@ enum Route {
 fn switch(routes: &Route) -> Html {
     match routes {
         Route::Home => html! { <Home/> },
-        Route::NotFound => html! { <h1>{ "404" }</h1> },
+        Route::NotFound => html! { <h1>{ "404 Not Found" }</h1> },
     }
 }
 
