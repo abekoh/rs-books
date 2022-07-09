ef6e744a083768395e8575bd20c7daf7051ce14d
Kotaro Abe <abekoh.bcky@gmail.com>
2022-07-09 13:45:33 +0900

    feat: show books

--- books_web/src/main.rs
+++ books_web/src/main.rs
@@ -1,9 +1,39 @@
+use serde::Deserialize;
+// use url::Url;
+// use uuid::Uuid;
 use yew::prelude::*;
+
+#[derive(Clone, PartialEq, Deserialize)]
+struct Book {
+    // pub id: Uuid,
+    pub name: String,
+    // pub url: Option<Url>,
+}
+
+#[derive(Properties, PartialEq)]
+struct BookListProps {
+    books: Vec<Book>,
+}
+
+#[function_component(BooksList)]
+fn books_list(BookListProps { books }: &BookListProps) -> Html {
+    books.iter().map(|book| {
+        html! {
+            <p>{book.name.clone()}</p>
+        }
+    }).collect()
+}
 
 #[function_component(App)]
 fn app() -> Html {
+    let books: Vec<Book> = vec![];
     html! {
-        <h1>{ "Hello World" }</h1>
+        <>
+            <h1>{ "Books" }</h1>
+            <div>
+                <BooksList books={books.clone()} />
+            </div>
+        </>
     }
 }
 
